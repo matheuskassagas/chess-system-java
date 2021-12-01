84452c7fc2b17a88575474583580508ad276ec65
Matheus Gigliotti <gigliotti_@hotmail.com>
2021-12-01 15:35:27 -0300

    Chess layer and printing the board

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,11 @@
+import chess.ChassMacth;
+import chess.ChessPiece;
+
 public class Program {
 
     public static void main (String[] args){
 
+        ChassMacth chassMacth = new ChassMacth();
+        UI.printBoard(chassMacth.getPieces());
     }
 }
