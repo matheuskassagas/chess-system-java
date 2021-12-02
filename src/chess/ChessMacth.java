da7ce65ba539d9129d416b9b39062430dada63c3
Matheus Gigliotti <gigliotti_@hotmail.com>
2021-12-02 14:35:20 -0300

    ChessException

--- src/chess/ChessMacth.java
+++ src/chess/ChessMacth.java
@@ -24,9 +24,13 @@ public class ChessMacth {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece chessPiece){
+        board.placePiece(chessPiece, new ChessPosition(column, row).toPosition());
+    }
+
     public void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
